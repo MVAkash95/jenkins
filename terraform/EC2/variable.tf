75032759748bea6bc0c66549966a07730c4f536d
Akash <akash.m@timingindia.com>
2025-08-05 14:25:45 +0530

    Code changes

---
+++ terraform/EC2/variable.tf
@@ -0,0 +1,18 @@
+variable "instance_type" {
+    type = string
+}
+variable "ami" {
+    type = string
+}
+variable "iam_instance_profile" {
+    type = string
+}
+variable "key_name" {
+  type = string
+}
+variable "subnet_id" {
+    type = string
+}
+variable "vpc_security_group_ids" {
+    type = list()
+}
