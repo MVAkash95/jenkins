6f6dfb9ffdddb8e19e2277553f71e1d8151ae4c8
Akash <akash.m@timingindia.com>
2025-08-06 15:51:10 +0530

    Infra changes

--- terraform/EC2/main.tf
+++ terraform/EC2/main.tf
@@ -2,15 +2,15 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = var.ami # us-east-1
-  instance_type = var.instance_type
-  iam_instance_profile = var.iam_instance_profile
-  key_name = var.key_name
-  subnet_id = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
-  tags = {
-      Name = "TF-Instance"
-  }
-}
+# resource "aws_instance" "foo" {
+#   ami           = var.ami # us-east-1
+#   instance_type = var.instance_type
+#   iam_instance_profile = var.iam_instance_profile
+#   key_name = var.key_name
+#   subnet_id = var.subnet_id
+#   vpc_security_group_ids = var.vpc_security_group_ids
+#   tags = {
+#       Name = "TF-Instance"
+#   }
+# }
 
