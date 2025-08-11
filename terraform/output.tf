46b93a5c80fd2aceb91b5ff7fc862cf69fb87925
Akash <akash.m@timingindia.com>
2025-08-11 16:51:55 +0530

    modified output.tf to get keyed by instance name

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,4 @@
 output "public_ip" {
-    value = [for inst in module.ec2 : inst.public_ip]
+    # value = [for inst in module.ec2 : inst.public_ip]
+    value = {for name, inst in module.ec2 : name => inst.public_ip}
 }
