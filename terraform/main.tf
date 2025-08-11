1c853f1fbe97aace6b10688fa1cdac0800ae6565
Akash <akash.m@timingindia.com>
2025-08-11 16:46:45 +0530

    updated code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 module "ec2" {
-    source = "./module/ec2"
-    for_each = var.instance
-    instance_type = each.value.instance_type
-    ami = each.value.ami
-    iam_instance_profile = each.value.iam_instance_profile
-    subnet_id = each.value.subnet_id
-    key_name = each.value.key_name
+    source                 = "./module/ec2"
+    for_each               = var.instance
+    instance_type          = each.value.instance_type
+    ami                    = each.value.ami
+    iam_instance_profile   = each.value.iam_instance_profile
+    subnet_id              = each.value.subnet_id
+    key_name               = each.value.key_name
     vpc_security_group_ids = each.value.vpc_security_group_ids
 }
